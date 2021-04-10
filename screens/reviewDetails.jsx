ca1d9bcdbc85e19f291f63b0be91f8dc5d92d2b2
Mako3 <big.mako@gmail.com>
2021-04-10 00:07:37 -0700

    passed object from home screen to review screen

--- screens/reviewDetails.jsx
+++ screens/reviewDetails.jsx
@@ -10,8 +10,9 @@ export default function ReviewDetails({ navigation }) {
 
     return(
         <View style={globalStyles.container}>
-            <Text style={globalStyles.titleText}>ReviewDetails</Text>
-            <Button title="back to home screen" onPress={pressHandler} />
+            <Text style={globalStyles.titleText}>{ navigation.getParam('title') }</Text>
+            <Text style={globalStyles.titleText}>{ navigation.getParam('body') }</Text>
+            <Text style={globalStyles.titleText}>{ navigation.getParam('rating') }</Text>
         </View>
     )
 }
