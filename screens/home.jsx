60f027c6db64f7cfcdd77de22f497ccf88cdb7a8
Mako3 <big.mako@gmail.com>
2021-04-09 23:56:13 -0700

    added flatlist and touchableOpacity

--- screens/home.jsx
+++ screens/home.jsx
@@ -1,19 +1,41 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Flatlist, TouchableOpacity } from "react-native";
 import { globalStyles } from "../styles/global";
 
 export default function Home({ navigation }) {
   const [reviews, setReviews] = useState([
-    { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'orem ipsum', key: '1'},
-    { title: 'Gotta Catch Them All (Again)', rating: 5, body: 'orem ipsum', key: '2'},
-    { title: 'Not So "Final" Fantasy', rating: 5, body: 'orem ipsum', key: '3'},
+    {
+      title: "Zelda, Breath of Fresh Air",
+      rating: 5,
+      body: "orem ipsum",
+      key: "1",
+    },
+    {
+      title: "Gotta Catch Them All (again)",
+      rating: 5,
+      body: "orem ipsum",
+      key: "2",
+    },
+    {
+      title: 'Not So "Final" Fantasy',
+      rating: 5,
+      body: "orem ipsum",
+      key: "3",
+    },
   ]);
 
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home Screen</Text>
-      <Text style={globalStyles.bodyText}>Home Screen</Text>
-      <Button title='go to review dets' onPress={pressHandler}/>
+      <Flatlist
+        data={reviews}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() => navigation.navigate("ReviewDetails", item)}
+          >
+            <Text style={globalStyles.titleText}>{item.title}</Text>
+          </TouchableOpacity>
+        )}
+      />
     </View>
   );
 }
